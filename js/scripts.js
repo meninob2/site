c73d91b28f48135db2467d8903f6e648222331b9
meninob2 <88125351+meninob2@users.noreply.github.com>
2021-08-23 17:19:26 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -41,6 +41,46 @@ const produtos = [
     }
 ];
 
+const scroll = (to, tempo = 500) => {
+    const position = document.documentElement.scrollTop
+
+    const dir = to > position
+
+    const dist = (dir ? 1 : -1) * (to - position)
+
+    const passos = []
+
+    let falta = 0
+
+    let dec = 1
+
+    while (falta < dist) {
+        if (falta + dec > dist) {
+            passos.push(dist)
+            falta = dist
+        } else {
+            falta += dec
+            passos.push(falta)
+            dec = dec + 2
+        }
+    }
+
+    const step = passos.map(e => position + (dir ? e : -e))
+    const stepTime = tempo / step.length
+
+    const next = (i = 0) => {
+
+        if (i > step.length - 1) return;
+
+        const e = step[i]
+        document.documentElement.scrollTop = e
+
+        setTimeout(() => next(i + 1), stepTime)
+    }
+
+    next()
+};
+
 function formatarPreco(valor) {
     const real = parseInt(valor).toString().replace(/(\d{1,3})(\d{3})$/, '$1.$2');
     const decimal = valor.toFixed(2).replace(/^\d+\./, '');
@@ -53,7 +93,26 @@ function formatarPreco(valor) {
 }
 
 window.addEventListener('load', () => {
+
     const ctProdutos = document.querySelector('#produtos .lista-produtos');
+    const menu = document.querySelector('#menu');
+    const btnAbreMenu = menu.querySelector('#abre-fecha-menu');
+
+    btnAbreMenu.addEventListener('click', () => {
+        menu.classList.toggle('aberto');
+    });
+
+    document.querySelectorAll('#menu ul li a').forEach(e => {
+        e.addEventListener('click', function (e) {
+            e.preventDefault()
+            menu.classList.remove('aberto')
+            const el = document.querySelector(this.getAttribute('href'))
+
+            if (el) {
+                scroll(el.offsetTop)
+            }
+        })
+    })
 
     produtos.forEach(item => {
         ctProdutos.innerHTML += `
